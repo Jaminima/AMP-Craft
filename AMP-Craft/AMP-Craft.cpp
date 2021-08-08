7b29cd01467079c00a0270270195f3d72dc85f63
Oscar Davies <oscar.william.davies@outlook.com>
2021-08-08 07:35:43 +0100

    Pick Accelerator

--- AMP-Craft/AMP-Craft.cpp
+++ AMP-Craft/AMP-Craft.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include "Window.h"
 
+bool pick_accelerator()
+{
+	std::vector<accelerator> accs = accelerator::get_all();
+	accelerator chosen_one;
+
+	auto result = std::find_if(accs.begin(), accs.end(),
+		[](const accelerator& acc)
+		{
+			return !acc.is_emulated &&
+				acc.supports_double_precision &&
+				!acc.has_display;
+		});
+
+	if (result != accs.end())
+	{
+		chosen_one = *(result);
+	}
+
+	std::wcout << chosen_one.description << std::endl;
+	bool success = accelerator::set_default(chosen_one.device_path);
+	return success;
+}
+
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World!\n";
-	/*SteppedRay r(Vec3(0, 0, 0), Vec3(0.2f, 0.5f, 0.7f));
-	Vec3 p;
-
-	while (true) {
-		p = r.GetNextPoint();
-	}*/
-
-	/*RayCaster r;
-	SteppedRay* rays = r.CreateAllViewRays();*/
+	pick_accelerator();
 
 	array_view<Cube, 3> world_arr(blocks_deep, blocks_long, blocks_wide, Renderer::world.cubeSet);
 
